a43afb22ea79da852b2bdb1e4810c6942f4b16a5
OliverSmith06 <52448893+OliverSmith06@users.noreply.github.com>
2021-07-07 04:25:19 +1000

    Basic Page
    Creating a layout for the header and navigation

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+test = '<h1>TESTING</h1>'
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', test=test)
 
 @app.route("/about")
 def about():
