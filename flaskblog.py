a4f80d17a30233c5ab1fcb713d54c4e657f98efc
OliverSmith06 <52448893+OliverSmith06@users.noreply.github.com>
2021-07-07 22:29:03 +1000

    subpagesAndLoadPages
    The website now includes indented sub pages (accessed by clicking on the main pages) and also the ability to update the main page through flask with information from JSON without reloading the page. Info is being passed from JS to the Python backend so flask can display the right page/post

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,47 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, session
+import requests
+import json
 app = Flask(__name__)
 
-test = '<h1>TESTING</h1>'
+test = {
+"title":"Valorant Go Knife",
+"h1":'test1',
+"h2":'test2',
+"h1": 'test3',
+"cars":["Ford", "BMW", "Fiat"]
+},{
+"title":"Steve",
+"p":"steve is a lovely chap",
+"age":22,
+"cars":["Ferrari", "Merc", "Toyota"]
+}
+
+title = 'Home'
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', test=test)
+    # print(request.args.get('title'))
+    return render_template('home.html', posts=test, title=title)
+
+@app.route("/modelling")
+def modelling():
+    return render_template('modelling.html', test=test)
+
+@app.route("/speechy")
+def speechy():
+    return render_template('speechy.html', test=test)
 
 @app.route("/about")
 def about():
     return "<h1>About Page</h1>"
+
+@app.route('/getPost', methods = ['POST'])
+def get_post_javascript_data():
+    global title
+    jsdata = request.form['javascript_data']
+    title = jsdata
+    return home()
 
 if __name__ == '__main__':
     app.run(debug=True)
